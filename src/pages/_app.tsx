4d1490c528a03be66b9c3b619e89ed73677883e0
Jon Lunde <jonlund95@gmail.com>
2022-06-15 22:39:27 +0200

    add ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,35 @@
 import "../styles/global.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+        });
+            `}
+      </Script>
+
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem={true}
+      >
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
