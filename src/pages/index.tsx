16911db374633f9b6084723a53ab385dc1e50f8f
JonMLunde <jonlund95@gmail.com>
2022-06-17 23:23:10 +0200

    favicon works locally now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,6 +44,26 @@ export default function IndexPage() {
             content="inneklemt, inneklemte, dager, ferie, fri, langhelg, helg, ovalweekend, oval, weekend"
           />
           <link rel="canonical" href="https://www.inneklemt.no" />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicons/favicon-16x16.png"
+          />
+          <meta name="theme-color" content="#151515" />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
         </Head>
         <Header
           selectedYear={selectedYear}
