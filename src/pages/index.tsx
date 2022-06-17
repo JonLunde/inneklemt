0eaeebeb0719466eccc7b07bac0c683257d49fa5
JonMLunde <jonlund95@gmail.com>
2022-06-17 22:01:12 +0200

    fix favicon href

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,21 +48,21 @@ export default function IndexPage() {
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/favicons/apple-touch-icon.png"
+            href="/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/favicons/favicon-32x32.png"
+            href="/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/favicons/favicon-16x16.png"
+            href="/favicon-16x16.png"
           />
-          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link rel="manifest" href="/site.webmanifest" />
           <meta name="theme-color" content="#0e0e0e" />
         </Head>
         <Header
